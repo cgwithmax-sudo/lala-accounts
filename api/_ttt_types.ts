e6622938e74dc542d9180cb0ad8d3cb6d3312d95
cgwithmax-sudo <cgwithmax@gmail.com>
2025-10-24 01:04:36 +0800

    Update _ttt_types.ts

--- api/_ttt_types.ts
+++ api/_ttt_types.ts
@@ -1,12 +1,11 @@
-// /api/_ttt_types.ts
 export type PlayerSymbol = 'X' | 'O';
 export type Cell = PlayerSymbol | '';
 
 export type RoomState = {
   id: string;
   createdAt: number;
-  board: Cell[];            // length 9
-  turn: PlayerSymbol;       // 'X' | 'O'
+  board: Cell[];
+  turn: PlayerSymbol;
   players: {
     X: { username: string; name: string } | null;
     O: { username: string; name: string } | null;
